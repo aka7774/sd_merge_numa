2682efee78b2d1879cb3fca580fc40ea3f8cb5ca
aka7774 <aka7774@users.noreply.github.com>
2022-12-27 03:07:02 +0900

    Fix Match model

--- scripts/merge_numa.py
+++ scripts/merge_numa.py
@@ -30,13 +30,17 @@ class Script(scripts.Script):
         images = []
         # 1行につき1モデルと1画像を作成する
         for line in batch.split("\n"):
+            print(f"Merge Numa Process: {line}")
             rs = line.rstrip("\r\n").split("\t")
 
             # unpack(ハッシュは対応しない)
             try:
                 model_0 = rs[0]
                 model_1 = rs[2]
-                output_file = os.path.join(sd_models.model_path, rs[4])
+                if shared.cmd_opts.ckpt_dir:
+                    output_file = os.path.join(shared.cmd_opts.ckpt_dir, rs[4])
+                else:
+                    output_file = os.path.join(sd_models.model_path, rs[4])
                 base_alpha = float(rs[6])
                 weights1 = rs[8]
             except:
@@ -57,12 +61,12 @@ class Script(scripts.Script):
 
             # ハイフンのあるディレクトリのインポート
             if not weights2:
-                # MBW
+                print("Log format: MBW (Classic)")
                 bw = importlib.import_module("extensions.sdweb-merge-block-weighted-gui.scripts.mbw.merge_block_weighted")
                 merge = getattr(bw, 'merge')
                 merge(weights1, model_0, model_1, device, base_alpha, output_file, allow_overwrite, verbose)
             else:
-                # MBW each
+                print("Log format: MBW Each")
                 bw = importlib.import_module("extensions.sdweb-merge-block-weighted-gui.scripts.mbw_each.merge_block_weighted_mod")
                 merge = getattr(bw, 'merge')
                 merge(weights1, weights2, model_0, model_1, device, base_alpha, output_file, allow_overwrite, verbose)
@@ -71,9 +75,22 @@ class Script(scripts.Script):
             sd_models.list_models()
 
             # マージできたモデルに切り替え
+            hit = False
             for c in sd_models.checkpoints_list.values():
-                if c.filename.find(output_file) != 1:
+                if c.filename == output_file:
+                    print(f"Use Model: {c.filename}")
                     p.override_settings['sd_model_checkpoint'] = c.title
+                    hit = True
+            if not hit:
+                # あいまいな検索
+                searchString = os.path.splitext(os.path.basename(output_file))[0]
+                c = sd_models.get_closet_checkpoint_match(searchString)
+                if c is not None:
+                    print(f"Matched Model: {c.filename}")
+                    p.override_settings['sd_model_checkpoint'] = c.title
+                    hit = True
+            if not hit:
+                raise ValueError(f"Model {output_file} not in checkpoints_list")
 
             # サンプルにログを残す
             p.extra_generation_params["Mergelog"] = line
